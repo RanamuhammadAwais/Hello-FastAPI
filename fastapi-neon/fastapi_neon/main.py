ea63840a4216fa18702a06769a75f53fbd868024
RanamuhammadAwais <ranamawais92@gmail.com>
2024-03-15 15:40:45 +0500

    FastAPi

--- fastapi-neon/fastapi_neon/main.py
+++ fastapi-neon/fastapi_neon/main.py
@@ -17,8 +17,8 @@ from fastapi import FastAPI
 class Todo(SQLModel, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True)
-
     content: str = Field(index=True)
+    name: str = Field(index=True)
 
 
 
